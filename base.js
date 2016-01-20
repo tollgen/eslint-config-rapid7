211dbedc6b1b8606f0938c35564e9e21f55ba5bc
Ben Tesser <btesser@rapid7.com>
2016-01-20 16:44:19 -0500

    Attempt to fix issues

--- base.js
+++ base.js
@@ -1,8 +1,9 @@
 module.exports = {
-  'extends': [
+  extends: [
       'eslint-config-rapid7/legacy',
       'eslint-config-rapid7/rules/es6',
-      'eslint-config-rapid7/rules/plugin'
-  ],
-  'rules': {}
+      'eslint-config-rapid7/rules/plugin',
+      'eslint-config-rapid7/rules/react'
+  ].map(require.resolve),
+  rules: {}
 };
