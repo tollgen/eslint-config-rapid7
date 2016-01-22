28baa107a2aac1bc6ada8c633c35479f39f414da
Ben Tesser <btesser@rapid7.com>
2016-01-22 16:37:24 -0500

    jsx-quotes rule

--- rules/react.js
+++ rules/react.js
@@ -6,6 +6,8 @@ module.exports = {
         'jsx': true
     },
     'rules': {
+        // specify whether double or single quotes should be used in JSX attributes
+        'jsx-quotes': [1, 'prefer-single'],
         // Prevent missing displayName in a React component definition
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
         'react/display-name': 2,
