1793640c0502494d2166f67aae6940f8d67b9d4a
Dave Greene <dgreene@rapid7.com>
2016-05-11 15:57:17 -0400

    Fix: React rules shouldn't be in the base config (#1)
    * Per the readme, the base config shouldn't lint React. Moved the react ruleset out of base and into index.
    
    * 1.1.0
    
    * Updated author and contributors.
    
    * Fixed email addresses. Put authors/contributors fields back to object form.

--- base.js
+++ base.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: [
       'eslint-config-rapid7/legacy',
-      'eslint-config-rapid7/rules/es6',
-      'eslint-config-rapid7/rules/react'
+      'eslint-config-rapid7/rules/es6'
   ].map(require.resolve),
   rules: {}
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
       'eslint-config-airbnb',
+      'eslint-config-rapid7/rules/react',
       'eslint-config-rapid7/base'
   ].map(require.resolve),
   rules: {}
