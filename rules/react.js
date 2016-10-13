ff6c58a686f11f295a5e4630323893d6fbad9741
Tony Quetano <tquetano-r7@users.noreply.github.com>
2016-10-13 16:47:38 -0400

    Updates... (#4)
    * Add several react-specific rules

--- rules/react.js
+++ rules/react.js
@@ -8,20 +8,33 @@ module.exports = {
   'rules': {
     'jsx-quotes': [2, 'prefer-double'],
 
+    'react/jsx-curly-spacing': [2, 'never'],
     'react/jsx-equals-spacing': [2, 'never'],
+    'react/jsx-filename-extension': [2, {
+      'extensions': [
+        '.js'
+      ]
+    }],
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-key': 2,
+    'react/jsx-max-props-per-line': [2, {
+      'maximum': 1
+    }],
     'react/jsx-no-duplicate-props': 2,
     'react/jsx-no-undef': 1,
     'react/jsx-sort-props': 2,
+    'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
+    'react/no-is-mounted': 2,
     'react/prefer-es6-class': 2,
+    'react/require-render-return': 2,
     'react/sort-comp': [1, {
       'order': [
         'lifecycle',
         'everything-else',
         'render'
       ]
-    }]
+    }],
+    'react/jsx-wrap-multilines': 2
   }
 };
